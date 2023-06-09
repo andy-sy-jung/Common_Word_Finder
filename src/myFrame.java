4d212f45a2665cca0c447ddc13589e48226b401e
Andy <andyjung5591@gmail.com>
2023-06-09 19:58:28 +0900

    Added ability to change the limit of words displayed, finished project

--- src/myFrame.java
+++ src/myFrame.java
@@ -1,21 +1,35 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDropEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 
 public class myFrame extends JFrame implements ActionListener {
 
     JButton fileChooserButton;
+    JButton increaseButton;
+    JButton decreaseButton;
     JTextArea textArea;
+    JLabel countLabel;
+    JLabel limitLabel;
+    int limit = 40;
+
     myFrame(){
         this.setTitle("Common Word Finder");
         this.setSize(800,800);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setResizable(false);
+
         //scrollPane start
         textArea = new JTextArea();
         textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
@@ -26,14 +40,14 @@ public class myFrame extends JFrame implements ActionListener {
         scrollPane.setBounds(400, 50, 375, 700);
         //scrollPane end
 
-        //scrollPane Label start
-        JLabel wordLabel = new JLabel();
-        wordLabel.setBounds(400, 22, 300, 100);
-        wordLabel.setText("Most Common Words");
-        wordLabel.setHorizontalAlignment(JLabel.LEFT);
-        wordLabel.setVerticalAlignment(JLabel.TOP);
-        wordLabel.setFont(new Font(wordLabel.getFont().getName(), Font.PLAIN, 18));
-        //scrollPane Label end
+        //countLabel start
+        countLabel = new JLabel();
+        countLabel.setBounds(400, 22, 300, 100);
+        countLabel.setText("Total Unique Words: ");
+        countLabel.setHorizontalAlignment(JLabel.LEFT);
+        countLabel.setVerticalAlignment(JLabel.TOP);
+        countLabel.setFont(new Font(countLabel.getFont().getName(), Font.PLAIN, 18));
+        //countLabel end
 
         //introLabel start
         JLabel introLabel = new JLabel();
@@ -50,8 +64,68 @@ public class myFrame extends JFrame implements ActionListener {
         fileChooserButton.setBounds(25, 80, 350, 50);
         fileChooserButton.addActionListener(this);
         //fileChooser end
+
+        //limitLabel start
+        limitLabel = new JLabel();
+        limitLabel.setBounds(30, 630, 250, 100);
+        limitLabel.setText("Limit: " + limit);
+        limitLabel.setFont(new Font(limitLabel.getFont().getName(), Font.PLAIN, 20));
+        //limitLabel end
+
+        //increaseButton start
+        increaseButton = new JButton("<html><div style='text-align: center;'>"
+                + "<font size='12'><b>&and;</b></font></div></html>");
+        increaseButton.setBounds(150, 650, 65, 60);
+        increaseButton.addActionListener(this);
+        //increaseButton end
+
+        //decreaseButton start
+        decreaseButton = new JButton("<html><div style='text-align: center;'>"
+                + "<font size='12'><b>&or;</b></font></div></html>");
+        decreaseButton.setBounds(225, 650, 65, 60);
+        decreaseButton.addActionListener(this);
+        //decreaseButton end
+
+        //DropBox start
+        Border border = BorderFactory.createLineBorder(Color.GRAY, 2);
+        JLabel dropLabel = new JLabel("Drag and drop file here.");
+        dropLabel.setBounds(25, 150, 350, 450);
+        dropLabel.setHorizontalAlignment(JLabel.CENTER);
+        dropLabel.setVerticalAlignment(JLabel.CENTER);
+        dropLabel.setBorder(border);
+        dropLabel.setDropTarget(new DropTarget(){
+            public synchronized void drop(DropTargetDropEvent evt) {
+                evt.acceptDrop(DnDConstants.ACTION_COPY);
+                Transferable transferable = evt.getTransferable();
+                if (transferable.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                    try {
+                        List<File> files = (List<File>) transferable.getTransferData(DataFlavor.javaFileListFlavor);
+                        if (files.size() == 1) {
+                            // Do something with the dropped file(s)
+                            String filePath = files.get(0).getAbsolutePath();
+                            Path path = Path.of(filePath);
+                            if(!isTxtFile(path)){
+                                invalidFileMessage();
+                            }
+                            else{
+                                handleDisplay(filePath);
+                            }
+                        }
+                    }
+                    catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+        //DropBox end
+
+        this.add(dropLabel);
         this.add(introLabel);
-        this.add(wordLabel);
+        this.add(limitLabel);
+        this.add(increaseButton);
+        this.add(decreaseButton);
+        this.add(countLabel);
         this.add(fileChooserButton);
         this.add(scrollPane);
         this.getContentPane().setBackground(new Color(161,158,157));
@@ -68,37 +142,88 @@ public class myFrame extends JFrame implements ActionListener {
             if(response == JFileChooser.APPROVE_OPTION){
                 Path path = Path.of(fileChooser.getSelectedFile().getAbsolutePath());
                 if(!isTxtFile(path)){
-                    //todo
+                    textArea.setText("File provided is not a valid text file.");
                 }
                 else{
-                    System.out.println("Text File Entered.");
-                    int limit = 20;
-                    String fileName = path.getFileName().toString();
-                    File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-                    MyMap<String, Integer> map = new MyHashMap<>();
-                    try{
-                        CommonWordFinder.parseAndStoreWords(file, map);
-                    }
-                    // catches an IO exception when reading
-                    catch(IOException er){
-                        //todo
-                        System.err.println("Error: An I/O error occurred reading " + fileName + ".");
-                    }
-                    Entry<String,Integer>[] arrayOfWords = CommonWordFinder.mostCommonWords(map);
-                    if(map.size() < limit){
-                        limit = map.size();
-                    }
-                    String displayText = CommonWordFinder.stringOfMostCommonWords(limit, arrayOfWords);
-                    textArea.setText(displayText);
-                    this.revalidate();
-                    this.repaint();
+                    handleDisplay(path, fileChooser);
                 }
+                this.revalidate();
+                this.repaint();
             }
         }
+
+        if(e.getSource()==increaseButton){
+            limit++;
+            limitLabel.setText("Limit: " + limit);
+            this.revalidate();
+            this.repaint();
+        }
+
+        if(e.getSource()==decreaseButton){
+            limit--;
+            limitLabel.setText("Limit: " + limit);
+            this.revalidate();
+            this.repaint();
+        }
+
     }
 
+    public void handleDisplay(Path path, JFileChooser fileChooser){
+        String fileName = path.getFileName().toString();
+        File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+        MyMap<String, Integer> map = new MyHashMap<>();
+        try{
+            CommonWordFinder.parseAndStoreWords(file, map);
+        }
+        // catches an IO exception when reading
+        catch(IOException er){
+            textArea.setText("Error: An I/O error occurred reading " + fileName + ".");
+            this.revalidate();
+            this.repaint();
+        }
+        Entry<String,Integer>[] arrayOfWords = CommonWordFinder.mostCommonWords(map);
+        if(map.size() < limit){
+            limit = map.size();
+        }
+        String displayText = CommonWordFinder.stringOfMostCommonWords(limit, arrayOfWords);
+        textArea.setText(displayText);
+        countLabel.setText("Total Unique Words: " + map.size());
+    }
+
+    public void handleDisplay(String filePath){
+        Path path = Path.of(filePath);
+        String fileName = path.getFileName().toString();
+        File file = new File(filePath);
+        MyMap<String, Integer> map = new MyHashMap<>();
+        try{
+            CommonWordFinder.parseAndStoreWords(file, map);
+        }
+        // catches an IO exception when reading
+        catch(IOException er){
+            textArea.setText("Error: An I/O error occurred reading " + fileName + ".");
+            this.revalidate();
+            this.repaint();
+        }
+        Entry<String,Integer>[] arrayOfWords = CommonWordFinder.mostCommonWords(map);
+        if(map.size() < limit){
+            limit = map.size();
+        }
+        String displayText = CommonWordFinder.stringOfMostCommonWords(limit, arrayOfWords);
+        textArea.setText(displayText);
+        countLabel.setText("Total Unique Words: " + map.size());
+        this.revalidate();
+        this.repaint();
+    }
     public boolean isTxtFile(Path path){
         String fileName = path.getFileName().toString();
         return fileName.endsWith(".txt");
     }
+
+    public void invalidFileMessage(){
+        textArea.setText("File provided is not a valid text file.");
+        this.revalidate();
+        this.repaint();
+    }
+
+
 }
